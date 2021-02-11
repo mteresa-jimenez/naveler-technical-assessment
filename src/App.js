859876de20239b48b3e109dc262bfa9249e5ab71
mteresa-jimenez <mteresa.jn@gmail.com>
2021-02-11 13:15:12 +0100

    Create React project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Hola mundo</header>
+      </div>
+    );
+  }
+}
+
+export default App;
