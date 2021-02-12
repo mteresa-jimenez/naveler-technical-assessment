bc0b6e787b28a4eb5fddef429c853b5c9a56f26c
mteresa-jimenez <mteresa.jn@gmail.com>
2021-02-12 10:19:42 +0100

    Render all flags

--- src/components/Flags.js
+++ src/components/Flags.js
@@ -1,5 +1,20 @@
 const Flags = (props) => {
-  return <div>Hola mundo</div>;
+  const Flag = props.countries.map((country, index) => {
+    return (
+      <li key={index}>
+        <img
+          src={"http://dev.naveler.com:8007/static/" + country.country_flag}
+          alt={"flag of " + country.name_en}
+        />
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <ul>{Flag}</ul>
+    </div>
+  );
 };
 
 export default Flags;
