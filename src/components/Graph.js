b2dfd78fc89b311b6ed21c2986bfb8985d4d532c
mteresa-jimenez <mteresa.jn@gmail.com>
2021-02-13 20:32:21 +0100

    Install highcharts

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,8 +1,30 @@
+import { render } from "react-dom";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import "../styles/Graph.scss";
+
+const options = {
+  title: {
+    text: "My chart",
+  },
+  series: [
+    {
+      data: [1, 2, 3],
+    },
+  ],
+};
 
 const Graph = (props) => {
   console.log("graph component", props.countries);
-  return <div className="graph">hola mundo</div>;
+
+  return (
+    <div className="graph">
+      hola mundo
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 };
+
+render(<Graph />, document.getElementById("root"));
 
 export default Graph;
