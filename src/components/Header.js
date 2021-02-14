35b6998dc43f7dd0ddad180f2f68ce9c7d5964e2
mteresa-jimenez <mteresa.jn@gmail.com>
2021-02-14 19:42:17 +0100

    Add styles to Header and Graph components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
+import "../styles/Header.scss";
 
 const Header = (props) => {
   return (
-    <>
-      <img src={logo} alt="Naveler logo" title="Naveler logo" />
-      <h1>{props.title}</h1>
+    <header className="header">
+      <h1 className="header--title">{props.title}</h1>
       <Link to="/">
-        <button> Go back to home </button>
+        <img
+          className="header--logo"
+          src={logo}
+          alt="Naveler logo"
+          title="Go back to home"
+        />
       </Link>
-    </>
+    </header>
   );
 };
 
