778b46c731babd4884db824f5136f7374eb234d6
mteresa-jimenez <mteresa.jn@gmail.com>
2021-02-14 12:33:31 +0100

    Merge dev branch into graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -3,23 +3,23 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import "../styles/Graph.scss";
 
-const options = {
-  title: {
-    text: "My chart",
-  },
-  series: [
-    {
-      data: [1, 2, 3],
-    },
-  ],
-};
-
 const Graph = (props) => {
   console.log("graph component", props.countries);
 
+  const options = {
+    title: {
+      text: "My chart",
+    },
+    series: [
+      {
+        data: [1, 2, 3],
+      },
+    ],
+  };
+
   return (
     <div className="graph">
-      hola mundo
+      <h1>chart</h1>
       <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
